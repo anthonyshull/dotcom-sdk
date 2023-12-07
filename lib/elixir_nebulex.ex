59d10dc7dee0fefc0296e245c944143a5f143f9f
Anthony Shull <shull.anthony@gmail.com>
2023-12-07 09:23:16 -0600

    initial commit

---
+++ lib/elixir_nebulex.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNebulex do
+  @moduledoc """
+  Documentation for `ElixirNebulex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNebulex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_nebulex_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNebulexTest do
+  use ExUnit.Case
+  doctest ElixirNebulex
+
+  test "greets the world" do
+    assert ElixirNebulex.hello() == :world
+  end
+end
