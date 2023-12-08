f00b9d98cb64d4ba6b1c3a912c468722f97ae056
Anthony Shull <shull.anthony@gmail.com>
2023-12-08 11:26:13 -0600

    remove runtime config from compile time config

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,3 @@ config :elixir_nebulex, ElixirNebulex.Cache,
 config :logster, format: :string
 
 config :mbta, base_url: "https://api-dev.mbtace.com"
-
-config :tesla, MBTA.Connection,
-  middleware: [{Tesla.Middleware.Headers, [{"x-api-key", System.get_env("V3_API_KEY")}]}]
